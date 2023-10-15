438821928bace1fe91d5cf243e1337e69d49351f
Alishan <146952690+Arsh4589@users.noreply.github.com>
2023-10-15 21:04:04 +0530

    food website project
    using html, css and javascript

---
+++ script.js
@@ -0,0 +1,5 @@
+alert("its my first responsive website. \nSo please ignore the design my main motive is to make it responsive :)")
+
+alert("Please upvote,comment and share if you like my work")
+
+alert("Any suggestion will be appericiated")
